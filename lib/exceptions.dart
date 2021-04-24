1f1d1ea2421977375fc77709d72f45248e36f411
Harpreet Sangar <happy_san@protonmail.com>
2021-04-24 10:24:36 +0530

    Add exceptions.

---
+++ lib/exceptions.dart
@@ -0,0 +1,11 @@
+class MissingConfiguration implements Exception {}
+
+class ObjectAlreadyExists implements Exception {}
+
+class ObjectNotFound implements Exception {}
+
+class ObjectUnprocessable implements Exception {}
+
+class RequestMalformed implements Exception {}
+
+class RequestUnauthorized implements Exception {}
