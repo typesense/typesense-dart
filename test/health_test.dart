dfe5b22191f18acc6ec86b0de79017d286575077
Harpreet Sangar <happy_san@protonmail.com>
2021-05-04 23:14:41 +0530

    Add Health.

---
+++ test/health_test.dart
@@ -0,0 +1,32 @@
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:typesense/src/api_call.dart';
+
+import 'package:typesense/src/health.dart';
+
+class MockApiCall extends Mock implements ApiCall {}
+
+void main() {
+  Health health;
+  MockApiCall mock;
+
+  setUp(() {
+    mock = MockApiCall();
+    health = Health(mock);
+  });
+
+  group('Health', () {
+    test('has a RESOURCEPATH', () {
+      expect(Health.RESOURCEPATH, equals('/health'));
+    });
+    test('retrieve() calls ApiCall.get()', () async {
+      when(
+        mock.get(
+          '/health',
+        ),
+      ).thenAnswer((realInvocation) => Future.value({"status": "ok"}));
+      expect(await health.retrieve(), equals({"status": "ok"}));
+    });
+  });
+}
