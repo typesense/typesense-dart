b3c5f22d63879499c1db68c61ad67758b3afe77f
Harpreet Sangar <happy_san@protonmail.com>
2021-05-04 23:17:04 +0530

    Add Metrics.

---
+++ lib/src/metrics.dart
@@ -0,0 +1,12 @@
+import 'api_call.dart';
+
+class Metrics {
+  final ApiCall _apiCall;
+  static const String RESOURCEPATH = '/metrics.json';
+
+  const Metrics(ApiCall apiCall) : _apiCall = apiCall;
+
+  Future<Map<String, dynamic>> retrieve() async {
+    return await _apiCall.get(RESOURCEPATH);
+  }
+}
