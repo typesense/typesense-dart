a46f0122c15cba73245428e4fbf043bfc0ad49ee
Harpreet Sangar <happy_san@protonmail.com>
2021-05-24 17:50:16 +0530

    Add MultiSearch.

---
+++ lib/src/multi_search.dart
@@ -0,0 +1,22 @@
+import './services/api_call.dart';
+
+class MultiSearch {
+  final ApiCall _apiCall;
+  final bool useTextContentType;
+  static const String RESOURCEPATH = '/multi_search';
+
+  const MultiSearch(ApiCall apiCall, {this.useTextContentType = false})
+      : _apiCall = apiCall;
+
+  Future<Map<String, dynamic>> perform(Map<String, dynamic> searchRequests,
+      {Map<String, String> queryParams}) async {
+    final additionalHeaders = <String, String>{};
+    if (useTextContentType) {
+      additionalHeaders[CONTENT_TYPE] = 'text/plain';
+    }
+    return await _apiCall.post(RESOURCEPATH,
+        bodyParameters: searchRequests,
+        queryParams: queryParams,
+        additionalHeaders: additionalHeaders);
+  }
+}
