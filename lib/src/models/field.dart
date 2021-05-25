2df5474df607aa7abc90b413040bb705efe3abdb
Harpreet Sangar <happy_san@protonmail.com>
2021-05-25 12:10:32 +0530

    Add `string*` type.

--- lib/src/models/field.dart
+++ lib/src/models/field.dart
@@ -1,9 +1,27 @@
 class Field {
+  /// [name] of the field.
   final String name;
+
+  /// [Type] of field.
   final Type type;
+
+  /// If this field can be ommited in a document.
   final bool isOptional;
+
+  /// If this field will be used in faceted search.
   final bool isFacetable;
+
+  /// If this field is an array containing multiple values.
+  ///
+  /// Applicable to the field with the [type]:
+  /// [Type.string], [Type.int32], [Type.int64], [Type.float] or [Type.bool]
   final bool isMultivalued;
+
+  /// Declaring a field as non-indexable
+  ///
+  /// Setting this field to `false` ensures it is not indexed. This is useful
+  /// when used along with auto schema detection.
+  final bool shouldIndex;
 
   Field(
     this.name,
@@ -11,6 +29,7 @@ class Field {
     this.isOptional = false,
     this.isFacetable = false,
     this.isMultivalued = false,
+    this.shouldIndex = true,
   }) {
     if (name == null || name.isEmpty) {
       throw ArgumentError('Ensure Field.name is set');
@@ -26,6 +45,7 @@ class Field {
     map['type'] = type.value(isMultivalued);
     map['facet'] = isFacetable;
     map['optional'] = isOptional;
+    map['index'] = shouldIndex;
     return map;
   }
 
@@ -49,13 +69,42 @@ class Field {
           isMultivalued == o.isMultivalued);
 }
 
-enum Type { string, int32, int64, float, bool, auto }
+/// Enumerates the allowed field types.
+///
+/// [Type.auto] and [Type.stringify] are special field types that are used for
+/// handling data sources with varying schema via automatic schema detection.
+///
+/// [Type.auto] is used to let Typesense detect the type of the fields
+/// automatically.
+///
+/// [Type.stringify] (`string*`) is a way to store the field value (both
+/// singular and multi-value/array values) as string.
+enum Type { string, int32, int64, float, bool, auto, stringify }
 
 extension _TypeExtension on Type {
   String value(bool isMultivalued) {
-    final description = this.toString(),
-        indexOfDot = description.indexOf('.'),
-        value = description.substring(indexOfDot + 1);
-    return isMultivalued ? value + '[]' : value;
+    switch (this) {
+      case Type.string:
+      case Type.int32:
+      case Type.int64:
+      case Type.float:
+      case Type.bool:
+        final description = this.toString(),
+            indexOfDot = description.indexOf('.'),
+            value = description.substring(indexOfDot + 1);
+
+        return isMultivalued ? value + '[]' : value;
+        break;
+
+      case Type.auto:
+        return 'auto';
+        break;
+      case Type.stringify:
+        return 'string*';
+        break;
+
+      default:
+        return '';
+    }
   }
 }
