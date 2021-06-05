e923d5d52a85fb8a9e4ebaafc6e69843e55dfeff
Harpreet Sangar <happy_san@protonmail.com>
2021-06-05 20:33:48 +0530

    Add `Key` and `Keys` (#53).

---
+++ lib/src/keys.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+
+import 'services/api_call.dart';
+
+class Keys {
+  final ApiCall _apiCall;
+  static const RESOURCEPATH = '/keys';
+
+  Keys(ApiCall apiCall) : _apiCall = apiCall;
+
+  Future<Map<String, dynamic>> create(Map<String, dynamic> params) async {
+    return await _apiCall.post(RESOURCEPATH, bodyParameters: params);
+  }
+
+  Future<Map<String, dynamic>> retrieve() async {
+    return await _apiCall.get(RESOURCEPATH);
+  }
+
+  static String generateScopedSearchKey(
+      String searchKey, Map<String, dynamic> parameters) {
+    final paramsJSON = json.encode(parameters),
+        hmacSha256 = Hmac(sha256, utf8.encode(searchKey)),
+        digest =
+            base64Encode(hmacSha256.convert(utf8.encode(paramsJSON)).bytes),
+        keyPrefix = searchKey.substring(0, 4),
+        rawScopedKey = '$digest$keyPrefix$paramsJSON';
+
+    return base64Encode(rawScopedKey.codeUnits);
+  }
+}
---
+++ test/keys_test.dart
@@ -0,0 +1,98 @@
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:typesense/src/keys.dart';
+import 'package:typesense/src/services/api_call.dart';
+
+class MockApiCall extends Mock implements ApiCall {}
+
+void main() {
+  Keys keys;
+  MockApiCall mock;
+
+  setUp(() {
+    mock = MockApiCall();
+    keys = Keys(mock);
+  });
+
+  group('Keys', () {
+    test('has a RESOURCEPATH', () {
+      expect(Keys.RESOURCEPATH, equals('/keys'));
+    });
+    test('create() calls Api.post()', () async {
+      when(mock.post('/keys', bodyParameters: {
+        'description': 'Search-only companies key.',
+        'actions': ['documents:search'],
+        'collections': ['companies']
+      })).thenAnswer((realInvocation) => Future.value({
+            "actions": ["*"],
+            "collections": ["*"],
+            "description": "Admin key.",
+            "id": 1,
+            "value": "k8pX5hD0793d8YQC5aD1aEPd7VleSuGP"
+          }));
+
+      expect(
+          await keys.create({
+            'description': 'Search-only companies key.',
+            'actions': ['documents:search'],
+            'collections': ['companies']
+          }),
+          equals({
+            "actions": ["*"],
+            "collections": ["*"],
+            "description": "Admin key.",
+            "id": 1,
+            "value": "k8pX5hD0793d8YQC5aD1aEPd7VleSuGP"
+          }));
+    });
+    test('retrieve() calls Api.get()', () async {
+      when(mock.get('/keys')).thenAnswer((realInvocation) => Future.value({
+            "keys": [
+              {
+                "actions": ["documents:search"],
+                "collections": ["users"],
+                "description": "Search-only key.",
+                "id": 1,
+                "value_prefix": "iKBT"
+              },
+              {
+                "actions": ["documents:search"],
+                "collections": ["users"],
+                "description": "Search-only key.",
+                "id": 2,
+                "value_prefix": "wst8"
+              }
+            ]
+          }));
+
+      expect(
+          await keys.retrieve(),
+          equals({
+            "keys": [
+              {
+                "actions": ["documents:search"],
+                "collections": ["users"],
+                "description": "Search-only key.",
+                "id": 1,
+                "value_prefix": "iKBT"
+              },
+              {
+                "actions": ["documents:search"],
+                "collections": ["users"],
+                "description": "Search-only key.",
+                "id": 2,
+                "value_prefix": "wst8"
+              }
+            ]
+          }));
+    });
+    test('has a generateScopedSearchKey static method', () {
+      expect(
+          Keys.generateScopedSearchKey('RN23GFr1s6jQ9kgSNg2O7fYcAUXU7127',
+              {'filter_by': 'company_id:124', 'expires_at': 1906054106}),
+          equals(
+              'OW9DYWZGS1Q1RGdSbmo0S1QrOWxhbk9PL2kxbTU1eXA3bCthdmE5eXJKRT1STjIzeyJmaWx0ZXJfYnkiOiJjb21wYW55X2lkOjEyNCIsImV4cGlyZXNfYXQiOjE5MDYwNTQxMDZ9'));
+    });
+  });
+}
