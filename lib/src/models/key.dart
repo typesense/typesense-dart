d25b4f0047193a6de2031791d3501f295fa35663
Harpreet Sangar <happy_san@protonmail.com>
2021-06-05 20:33:48 +0530

    Add `Key` and `Keys` (#53).

---
+++ lib/src/models/key.dart
@@ -0,0 +1,21 @@
+import '../keys.dart';
+import '../services/api_call.dart';
+
+class Key {
+  final int _id;
+  final ApiCall _apiCall;
+
+  Key(int id, ApiCall apiCall)
+      : _id = id,
+        _apiCall = apiCall;
+
+  Future<Map<String, dynamic>> retrieve() async {
+    return await _apiCall.get(_endpointPath);
+  }
+
+  Future<Map<String, dynamic>> delete() async {
+    return await _apiCall.delete(_endpointPath);
+  }
+
+  String get _endpointPath => '${Keys.RESOURCEPATH}/$_id';
+}
---
+++ test/models/key_test.dart
@@ -0,0 +1,44 @@
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:typesense/src/models/key.dart';
+import 'package:typesense/src/services/api_call.dart';
+
+class MockApiCall extends Mock implements ApiCall {}
+
+void main() {
+  group('Key', () {
+    Key key;
+
+    MockApiCall mock;
+    setUp(() {
+      mock = MockApiCall();
+      key = Key(1, mock);
+    });
+    test('retrieve() calls Api.get()', () async {
+      when(mock.get('/keys/1')).thenAnswer((realInvocation) => Future.value({
+            "actions": ["documents:search"],
+            "collections": ["*"],
+            "description": "Search-only key.",
+            "id": 1,
+            "value_prefix": "vxpx"
+          }));
+
+      expect(
+          await key.retrieve(),
+          equals({
+            "actions": ["documents:search"],
+            "collections": ["*"],
+            "description": "Search-only key.",
+            "id": 1,
+            "value_prefix": "vxpx"
+          }));
+    });
+    test('delete() calls Api.delete()', () async {
+      when(mock.delete('/keys/1'))
+          .thenAnswer((realInvocation) => Future.value({"id": 1}));
+
+      expect(await key.delete(), equals({"id": 1}));
+    });
+  });
+}
