6ad272d90428f9960da4a5c2516e26f46fbcf320
Harpreet Sangar <happy_san@protonmail.com>
2021-06-06 13:09:58 +0530

    Expose `SearchClient`.

--- lib/typesense.dart
+++ lib/typesense.dart
@@ -1,6 +1,7 @@
 library typesense;
 
 export 'src/client.dart';
+export 'src/search_client.dart';
 export 'src/configuration.dart';
 export 'src/keys.dart' show generateScopedSearchKey;
 export 'src/models/models.dart';
