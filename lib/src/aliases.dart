e02ef160dc1766ead559207db728d54026486c58
Harpreet Sangar <happy_san@protonmail.com>
2021-07-05 15:39:41 +0530

    Add doc comments and example for Aliases. (#68)

--- lib/src/aliases.dart
+++ lib/src/aliases.dart
@@ -6,6 +6,7 @@ class Aliases {
 
   Aliases(ApiCall apicall) : _apiCall = apicall;
 
+  /// Creates/updates an alias corresponding to [aliasName].
   Future<Map<String, dynamic>> upsert(
       String aliasName, Map<String, String> mapping) async {
     return await _apiCall.put(
@@ -14,6 +15,7 @@ class Aliases {
     );
   }
 
+  /// Retrieves all aliases and the corresponding collections that they map to.
   Future<Map<String, dynamic>> retrieve() async {
     return await _apiCall.get(RESOURCEPATH);
   }
