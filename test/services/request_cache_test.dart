a85a83d0a6d405d2ef0ddc842064d8eceb9ce3bd
Harpreet Sangar <happy_san@protonmail.com>
2021-08-20 14:17:49 +0530

    Add test for LRU eviction. [skip ci]

--- test/services/request_cache_test.dart
+++ test/services/request_cache_test.dart
@@ -12,18 +12,28 @@ import '../test_utils.dart';
 
 class MockResponse extends Mock implements http.Response {}
 
+Future<Map<String, dynamic>> send(request) async {
+  final response = await request(
+    Node(
+      protocol: protocol,
+      host: host,
+      path: pathToService,
+    ),
+  );
+  return json.decode(response.body);
+}
+
 void main() {
   RequestCache requestCache;
   MockResponse mockResponse;
   int requestNumber;
-  Send<Map<String, dynamic>> send;
   Request request;
 
   setUp(() {
-    requestCache = RequestCache(5, Duration(seconds: 1));
+    requestCache = RequestCache(5, Duration(seconds: 1), send);
+
     mockResponse = MockResponse();
     requestNumber = 1;
-
     when(mockResponse.body).thenAnswer((invocation) {
       switch (requestNumber++) {
         case 1:
@@ -36,17 +46,9 @@ void main() {
           return json.encode({});
       }
     });
-
-    send = (request) async {
-      final response = await request(
-          Node(protocol: protocol, host: host, path: pathToService));
-      return json.decode(response.body);
-    };
     request = (node) => Future.value(mockResponse);
   });
   group('RequestCache', () {
-    final requestCache = RequestCache(5, Duration(seconds: 1));
-
     test('has a size field', () {
       expect(requestCache.size, equals(5));
     });
@@ -57,10 +59,12 @@ void main() {
       expect(
           await requestCache.getResponse(
             '/value'.hashCode,
-            send,
             request,
           ),
           equals({'value': 'initial'}));
+    });
+    test('has a send method', () async {
+      expect(await requestCache.send(request), equals({'value': 'initial'}));
     });
   });
 
@@ -69,14 +73,12 @@ void main() {
       expect(
           await requestCache.getResponse(
             '/value'.hashCode,
-            send,
             request,
           ),
           equals({'value': 'initial'}));
       expect(
           await requestCache.getResponse(
             '/value'.hashCode,
-            send,
             request,
           ),
           equals({'value': 'initial'}));
@@ -85,14 +87,12 @@ void main() {
       expect(
           await requestCache.getResponse(
             '/value'.hashCode,
-            send,
             request,
           ),
           equals({'value': 'initial'}));
       expect(
           await requestCache.getResponse(
             '/value'.hashCode,
-            send,
             request,
           ),
           equals({'value': 'initial'}));
@@ -101,10 +101,64 @@ void main() {
       expect(
           await requestCache.getResponse(
             '/value'.hashCode,
-            send,
             request,
           ),
           equals({'value': 'updated'}));
+    });
+    test('evicts the least recently used response', () async {
+      requestCache = RequestCache(5, Duration(seconds: 10), send);
+
+      final mockResponses = List.generate(6, (_) => MockResponse()),
+          callCounters = List.filled(6, 0);
+      var i = 0;
+
+      for (final mockResponse in mockResponses) {
+        when(mockResponse.body).thenAnswer((invocation) {
+          return json.encode({'$i': '${++callCounters[i]}'});
+        });
+      }
+
+      // Cache size is 5, filling up the cache with different responses.
+      for (; i < 5; i++) {
+        expect(
+            await requestCache.getResponse(
+              i,
+              (node) => Future.value(mockResponses[i]),
+            ),
+            equals({'$i': '1'}));
+      }
+
+      // The responses should still be 1 since they're cached.
+      i = 0;
+      for (; i < 5; i++) {
+        expect(
+            await requestCache.getResponse(
+              i,
+              (node) => Future.value(mockResponses[i]),
+            ),
+            equals({'$i': '1'}));
+      }
+
+      // Least recently used response at this moment should be index 0 and hence
+      // should be evicted by the following call.
+      expect(
+          await requestCache.getResponse(
+            5,
+            (node) => Future.value(mockResponses[5]),
+          ),
+          equals({'5': '1'}));
+
+      // The responses should now be 2 since each response gets evicted before
+      // being called again.
+      i = 0;
+      for (; i < 5; i++) {
+        expect(
+            await requestCache.getResponse(
+              i,
+              (node) => Future.value(mockResponses[i]),
+            ),
+            equals({'$i': '2'}));
+      }
     });
   });
 }
