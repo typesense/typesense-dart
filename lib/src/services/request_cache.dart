4eaef8ac50bb7f2a1c4e9662161a87d9ce9a36c4
Harisaran <hari@luxecraft.org>
2021-08-22 15:32:20 +0530

    add: lru cache

--- lib/src/services/request_cache.dart
+++ lib/src/services/request_cache.dart
@@ -1,42 +1,33 @@
 import 'dart:collection';
+import 'package:dcache/dcache.dart';
+import 'package:http/http.dart';
 
-import 'typedefs.dart';
+import 'typedefs.dart' as defs;
 
 /// Cache store which uses a [HashMap] internally to serve requests.
 class RequestCache {
-  final _cachedResponses = HashMap<int, _Cache>();
+  Cache _cachedResponses;
+  final Duration timeToUse;
+  final int size;
+  final defs.Send<Map<String, dynamic>> send;
+
+  RequestCache(this.size, this.timeToUse, this.send) {
+    _cachedResponses = LruCache<dynamic, Response>(storage: InMemoryStorage(size));
+  }
 
   // TODO(harisarang): rename this function to getResponse
   /// Caches the response of the [request], identified by [key]. The cached
   /// response is valid till [cacheTTL].
-  Future<Map<String, dynamic>> cache(
+  Future<Map<String, dynamic>> getResponse(
     int key,
-    Send<Map<String, dynamic>> send, // Only being used by ApiCall for now.
-    Request request,
-    Duration cacheTTL,
+    defs.Request request,
   ) async {
     if (_cachedResponses.containsKey(key)) {
-      if (_isCacheValid(_cachedResponses[key], cacheTTL)) {
-        // Cache entry is still valid, return it
-        return Future.value(_cachedResponses[key].data);
-      } else {
-        // Cache entry has expired, so delete it explicitly
-        _cachedResponses.remove(key);
-      }
+      return send(_cachedResponses.get(key));
     }
-
-    final response = await send(request);
-    _cachedResponses[key] = _Cache(response, DateTime.now());
+    
+    var response = await send(request);
+    _cachedResponses.set(key, response);
     return response;
   }
-
-  bool _isCacheValid(_Cache cache, Duration cacheTTL) =>
-      DateTime.now().difference(cache.creationTime) < cacheTTL;
-}
-
-class _Cache {
-  final DateTime creationTime;
-  final Map<String, dynamic> data;
-
-  const _Cache(this.data, this.creationTime);
 }
