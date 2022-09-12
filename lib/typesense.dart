fc09a4e8a91620f8d53127afc17e88fd57693452
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2022-09-12 23:57:20 +0400

    add exports

--- lib/typesense.dart
+++ lib/typesense.dart
@@ -4,4 +4,6 @@ export 'src/client.dart';
 export 'src/search_client.dart';
 export 'src/configuration.dart';
 export 'src/models/models.dart';
+export 'src/collection.dart';
+export 'src/collections.dart';
 export 'src/exceptions/exceptions.dart';
