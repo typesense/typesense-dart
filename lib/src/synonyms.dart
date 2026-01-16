ce3bf2c4cb5e066ce8a59277a33d31c9b96cd7b3
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-01-16 21:09:41 +0200

    fix(synonyms): add deprecation message to old synonyms

--- lib/src/synonyms.dart
+++ lib/src/synonyms.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'collections.dart';
 import 'services/api_call.dart';
 
@@ -5,21 +7,37 @@ class Synonyms {
   final String _collectionName;
   final ApiCall _apiCall;
   static const resourcepath = '/synonyms';
+  static bool _warnedDeprecated = false;
 
   const Synonyms(String collectionName, ApiCall apiCall)
       : _collectionName = collectionName,
         _apiCall = apiCall;
 
+  static void _warnDeprecated() {
+    if (_warnedDeprecated) {
+      return;
+    }
+    _warnedDeprecated = true;
+    stderr.writeln(
+      "[typesense] 'Synonyms' is deprecated on Typesense Server v30+. "
+      "Use client.synonymSets instead.",
+    );
+  }
+
   /// Creates/updates a synonym corresponding to [synonymId].
   Future<Map<String, dynamic>> upsert(
       String synonymId, Map<String, dynamic> params) async {
-    return await _apiCall.put('$_endpointPath/$synonymId',
+    _warnDeprecated();
+    final response = await _apiCall.put('$_endpointPath/$synonymId',
         bodyParameters: params);
+    return Map<String, dynamic>.from(response);
   }
 
   /// Retrieves all synonyms.
   Future<Map<String, dynamic>> retrieve() async {
-    return await _apiCall.get(_endpointPath);
+    _warnDeprecated();
+    final response = await _apiCall.get(_endpointPath);
+    return Map<String, dynamic>.from(response);
   }
 
   String get _endpointPath =>
