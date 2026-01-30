de38f1fca92299c201f536dd77de53d54551a8b3
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-01-30 17:28:43 +0200

    feat(schema): add missing field and collection schema properties
    - add field properties: stem, store, range_index, token_separators,
      symbols_to_index, stem_dictionary, truncate_len
    - add schema properties: created_at, num_memory_shards, curation_sets,
      synonym_sets, symbols_to_index, token_separators
    - add image type to Field Type enum

--- lib/src/models/schema.dart
+++ lib/src/models/schema.dart
@@ -31,12 +31,36 @@ class Schema extends BaseSchema {
   /// Boolean to enable nested fields on the schema, only available for typesense 0.24 or more
   final bool? enableNestedFields;
 
+  /// Timestamp when the collection was created.
+  final int? createdAt;
+
+  /// Number of memory shards used by the collection.
+  final int? numMemoryShards;
+
+  /// List of curation set names associated with this collection.
+  final List<String>? curationSets;
+
+  /// List of synonym set names associated with this collection.
+  final List<String>? synonymSets;
+
+  /// List of special characters to index at the collection level.
+  final List<String>? symbolsToIndex;
+
+  /// List of characters to use as token separators at the collection level.
+  final List<String>? tokenSeparators;
+
   Schema(
     this.name,
     super.fields, {
     this.defaultSortingField,
     this.documentCount,
     this.enableNestedFields,
+    this.createdAt,
+    this.numMemoryShards,
+    this.curationSets,
+    this.synonymSets,
+    this.symbolsToIndex,
+    this.tokenSeparators,
   });
 
   factory Schema.fromMap(Map<String, dynamic> map) {
@@ -66,6 +90,12 @@ class Schema extends BaseSchema {
       documentCount: map['num_documents'] ?? 0,
       defaultSortingField: defaultSortingField,
       enableNestedFields: enableNestedFields,
+      createdAt: map['created_at'],
+      numMemoryShards: map['num_memory_shards'],
+      curationSets: (map['curation_sets'] as List?)?.cast<String>(),
+      synonymSets: (map['synonym_sets'] as List?)?.cast<String>(),
+      symbolsToIndex: (map['symbols_to_index'] as List?)?.cast<String>(),
+      tokenSeparators: (map['token_separators'] as List?)?.cast<String>(),
     );
   }
 
@@ -82,12 +112,48 @@ class Schema extends BaseSchema {
     if (enableNestedFields != null) {
       map['enable_nested_fields'] = enableNestedFields;
     }
+    if (createdAt != null) {
+      map['created_at'] = createdAt;
+    }
+    if (numMemoryShards != null) {
+      map['num_memory_shards'] = numMemoryShards;
+    }
+    if (curationSets != null) {
+      map['curation_sets'] = curationSets;
+    }
+    if (synonymSets != null) {
+      map['synonym_sets'] = synonymSets;
+    }
+    if (symbolsToIndex != null) {
+      map['symbols_to_index'] = symbolsToIndex;
+    }
+    if (tokenSeparators != null) {
+      map['token_separators'] = tokenSeparators;
+    }
     return map;
   }
 }
 
 class UpdateSchema extends BaseSchema {
-  UpdateSchema(Set<UpdateField> super.fields);
+  /// List of synonym set IDs to associate with the collection.
+  final List<String>? synonymSets;
+
+  /// List of curation set IDs to associate with the collection.
+  final List<String>? curationSets;
+
+  /// List of special characters to index at the collection level.
+  final List<String>? symbolsToIndex;
+
+  /// List of characters to use as token separators at the collection level.
+  final List<String>? tokenSeparators;
+
+  UpdateSchema(
+    Set<UpdateField> super.fields, {
+    this.synonymSets,
+    this.curationSets,
+    this.symbolsToIndex,
+    this.tokenSeparators,
+  });
 
   factory UpdateSchema.fromMap(Map<String, dynamic> map) {
     final Set<UpdateField> fields = (map['fields'] != null)
@@ -98,7 +164,29 @@ class UpdateSchema extends BaseSchema {
 
     return UpdateSchema(
       fields,
+      synonymSets: (map['synonym_sets'] as List?)?.cast<String>(),
+      curationSets: (map['curation_sets'] as List?)?.cast<String>(),
+      symbolsToIndex: (map['symbols_to_index'] as List?)?.cast<String>(),
+      tokenSeparators: (map['token_separators'] as List?)?.cast<String>(),
     );
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    final map = super.toMap();
+    if (synonymSets != null) {
+      map['synonym_sets'] = synonymSets;
+    }
+    if (curationSets != null) {
+      map['curation_sets'] = curationSets;
+    }
+    if (symbolsToIndex != null) {
+      map['symbols_to_index'] = symbolsToIndex;
+    }
+    if (tokenSeparators != null) {
+      map['token_separators'] = tokenSeparators;
+    }
+    return map;
   }
 }
 
